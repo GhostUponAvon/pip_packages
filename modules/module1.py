33e3aace8035e6348b7239c8e4e7de90f0350012
GhostUponAvon <79240120+GhostUponAvon@users.noreply.github.com>
2021-10-18 20:16:19 +1000

    Update module1.py

--- modules/module1.py
+++ modules/module1.py
@@ -1,6 +1,11 @@
 def hw():
     print('hello world')
     
-
-    
-test = 'hello'
+def lettercount(doc, spaces='yes'):
+    num = 0
+    for i in doc :
+        if i != ' ' and spaces == 'no':
+            num = num + 1
+        elif spaces == 'yes':
+            num += 1    
+    return num
