93eb94d6c6b1e2255e90de44e86462a1c93b7e37
GhostUponAvon <79240120+GhostUponAvon@users.noreply.github.com>
2021-10-18 21:17:07 +1000

    Update __init__.py

--- modules/__init__.py
+++ modules/__init__.py
@@ -1,8 +1,7 @@
 '''info that can be called'''
 
-_version_ = "v0.0.4"
+_version_ = "v0.0.5"
 _author_ = "GhostUponAvon"
 _credits_ = "Visual Studio Code"
 
-import letters
-import words
+from .module1 import lettercount
