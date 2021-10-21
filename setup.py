1fcebbc45ad331a263a608bf85c5ade371ff98f5
GhostUponAvon <79240120+GhostUponAvon@users.noreply.github.com>
2021-10-21 20:27:23 +1000

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name='modules',
-    version='0.0.8',    
+    version='0.0.9',    
     description='Testing the package',
     url='https://github.com/GhostUponAvon/pip_packages',
     author='GhostUponAvon',
