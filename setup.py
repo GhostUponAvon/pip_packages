76fb7597f6c5992dfb6f8c513b66c8ca03ba8d4f
GhostUponAvon <79240120+GhostUponAvon@users.noreply.github.com>
2021-11-04 08:02:58 +1000

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name='directory-maker',
-    version='0.1.3',    
+    name='dirmaker',
+    version='0.1.4',    
     description='Testing the package',
     url='https://github.com/GhostUponAvon/pip_packages',
     author='GhostUponAvon',
     author_email='n/a',
     license='MIT',
-    packages=['directory-maker'],
+    packages=['dirmaker'],
     long_description=long_description,
     long_description_content_type="text/markdown",
     package_data={'': ['*.cmd']}    
