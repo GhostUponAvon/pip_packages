257112e7c61800d01ba8e1ef528ea4cf2bf7760d
GhostUponAvon <79240120+GhostUponAvon@users.noreply.github.com>
2021-11-12 09:30:30 +1000

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name='dirmaker',
-    version='0.1.8',    
+    version='0.1.9',    
     description='Testing the package',
     url='https://github.com/GhostUponAvon/pip_packages',
     author='GhostUponAvon',
