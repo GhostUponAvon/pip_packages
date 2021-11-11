a3f3ab89cb96f469d4cbbbdb744024bdfe054d9d
GhostUponAvon <79240120+GhostUponAvon@users.noreply.github.com>
2021-11-12 09:28:07 +1000

    Update createshortcut.py

--- dirmaker/createshortcut.py
+++ dirmaker/createshortcut.py
@@ -1,39 +1,24 @@
 import os, winshell
 from win32com.client import Dispatch
 
-
 desktop = winshell.desktop() #base directory for desktop
-
 
 path = os.path.join(desktop, "Run Directory Maker.lnk")  # the shortcuts path
 
-path2 = os.getcwd() #get the current directory
+wDir = os.getcwd() #get the current directory
 
-path_w_ext = os.path.join(path2, "runapp.cmd")
+target = os.path.join(wDir, "runapp.cmd")            
 
-
-target = path_w_ext
-
-
-wDir = path2                 
-
-
-icon = "icon.ico"
-
+iconpath = os.path.join(wDir, "icon.ico")
 
 shell = Dispatch('WScript.Shell') #dunno what this does
 
-
 shortcut = shell.CreateShortCut(path) #created shortcut 
-
 
 shortcut.Targetpath = target
 
-
 shortcut.WorkingDirectory = wDir
 
-
-shortcut.IconLocation = icon  
-
+shortcut.IconLocation = iconpath
 
 shortcut.save() #saves the shortcut
